93a27b62ba83094712435ddbb30789e8c6643929
gorakgarak mackbook <kohry@gorakgarakui-macbook-pro.local>
2018-05-02 21:08:56 +0900

    Initial Commit

---
+++ waefawefwef/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  waefawefwef
+//
+//  Created by gorakgarak mackbook on 2018. 5. 2..
+//  Copyright © 2018년 kancinsoft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
