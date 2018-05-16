fef9293175349221656d7a0dd0828a0917242886
gorakgarak mackbook <kohry@gorakgarakui-macbook-pro.local>
2018-05-16 22:18:43 +0900

    test

--- waefawefwef/ViewController.swift
+++ waefawefwef/ViewController.swift
@@ -8,8 +8,14 @@
 
 import Cocoa
 
+import Vision
+
 class ViewController: NSViewController {
 
+    @IBOutlet weak var desc: NSTextField!
+    @IBOutlet weak var path: NSTextField!
+    @IBOutlet weak var image: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +28,62 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func readImageFiles(_ sender: Any) {
+        
+        //open file with file manager
+        let fileManager = FileManager.default
+        
+        let contents = try fileManager.contentsOfDirectory(atPath: path.stringValue)
+        let urls = contents.map { analyze( $0 as? Image ) }
+        
+        //for loop for analyzing faces.
+        
+        
+    }
+    
+    
+    func analyze(_ image: Any) {
+        
+        let request = VNDetectFaceRectanglesRequest { (req, error) in
+            if let error = error {
+                print("Failed to detect faces", error)
+                //alert
+                return
+            }
+            
+            guard let observation = req.results as? [VNFaceObservation]
+                else { fatalError("Unexpected Result Type") }
+            
+            observation.forEach({ (observation) in
+                
+                DispatchQueue.main.async {
+                    print(observation.boundingBox)
+                    
+                }
+                
+            })
+            
+        }
+        
+        guard let cgImage = image.cgImage else { return }
+        
+        DispatchQueue.global(qos: .background).async {
+            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            
+            do {
+                try handler.perform([request])
+            } catch let reqError {
+                print("Error in req",reqError)
+            }
+        }
+        
+    }
+    
+    @IBAction func export(_ sender: Any) {
+    }
+    
+    @IBAction func close(_ sender: Any) {
+    }
+    
 }
 
