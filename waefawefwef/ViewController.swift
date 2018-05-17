fd43e82e808ad331be2bd0226562c66d488fef4c
gorakgarak mackbook <kohry@gorakgarakui-macbook-pro.local>
2018-05-17 22:34:17 +0900

    working source

--- waefawefwef/ViewController.swift
+++ waefawefwef/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        path.stringValue = "/Users/kohry/Documents/faceimage.png"
 
         // Do any additional setup after loading the view.
     }
@@ -33,16 +35,45 @@ class ViewController: NSViewController {
         //open file with file manager
         let fileManager = FileManager.default
         
-        let contents = try fileManager.contentsOfDirectory(atPath: path.stringValue)
-        let urls = contents.map { analyze( $0 as? Image ) }
+        //let contents = try! fileManager.contentsOfDirectory(atPath: path.stringValue)
+        //contents.map { analyze( $0 as! CGImage ) }
+//        analyze( contents as! CGImage )
         
+        let str = path.stringValue
+        
+        if fileManager.fileExists(atPath: str) {
+            
+            let data = try! Data(contentsOf: URL(fileURLWithPath: str))
+            
+            let ciimage = CIImage(contentsOf: URL(fileURLWithPath: str))
+            
+            
+//            let ciimage = CIImage(data: Data(contentsOf: URL(fileURLWithPath: path.stringValue)))
+            
+            
+            let context = CIContext(options: nil)
+            let cgimage = context.createCGImage(ciimage!, from: ciimage!.extent)
+            analyze(cgimage!)
+        }
+        
+        
+        
+//        if fileManager.fileExists(atPath: path.stringValue) {
+//            let url2 = URL.init(string: "sibal")
+//            let url = URL(string: path.stringValue)!
+//            CIImage(data: Data("sibal"))
+//
+//
+//            analyze(image)
+//        }
+//
         //for loop for analyzing faces.
         
         
     }
     
     
-    func analyze(_ image: Any) {
+    func analyze(_ image: CGImage) {
         
         let request = VNDetectFaceRectanglesRequest { (req, error) in
             if let error = error {
@@ -50,32 +81,47 @@ class ViewController: NSViewController {
                 //alert
                 return
             }
-            
-            guard let observation = req.results as? [VNFaceObservation]
-                else { fatalError("Unexpected Result Type") }
-            
-            observation.forEach({ (observation) in
-                
-                DispatchQueue.main.async {
-                    print(observation.boundingBox)
-                    
-                }
-                
-            })
-            
+            guard let observation = req.results as? [VNFaceObservation] else { fatalError("Unexpected Result Type") }
+            observation.forEach({ (observation) in DispatchQueue.main.async { print(observation.boundingBox) } })
         }
-        
-        guard let cgImage = image.cgImage else { return }
         
         DispatchQueue.global(qos: .background).async {
-            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-            
-            do {
-                try handler.perform([request])
-            } catch let reqError {
-                print("Error in req",reqError)
+            let handler = VNImageRequestHandler(cgImage: image, options: [:])
+            do { try handler.perform([request]) } catch let reqError { print("Error in req",reqError) }
+        }
+        
+        let faceLandmarks = VNDetectFaceLandmarksRequest()
+        let faceLandmarksDetectionRequest = VNSequenceRequestHandler()
+        
+        try? faceLandmarksDetectionRequest.perform([faceLandmarks], on: image)
+        if let landmarksResults = faceLandmarks.results as? [VNFaceObservation] {
+            for observation in landmarksResults {
+                DispatchQueue.main.async {
+                    if let boundingBox = faceLandmarks.inputFaceObservations?.first?.boundingBox {
+                        
+                        //different types of landmarks
+                        let faceContour = observation.landmarks?.faceContour
+                        
+                        let leftEye = observation.landmarks?.leftEye
+                        
+                        let rightEye = observation.landmarks?.rightEye
+                        
+                        let nose = observation.landmarks?.nose
+                        
+                        let lips = observation.landmarks?.innerLips
+                        
+                        let leftEyebrow = observation.landmarks?.leftEyebrow
+                        
+                        let rightEyebrow = observation.landmarks?.rightEyebrow
+                        
+                        let noseCrest = observation.landmarks?.noseCrest
+                        
+                        let outerLips = observation.landmarks?.outerLips
+                    }
+                }
             }
         }
+        
         
     }
     
